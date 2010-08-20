67d77a566b37a2b80d41259246acfd01a7be22d8
Gabriel Horner <gabriel.horner@gmail.com>
2010-08-20 04:24:54 -0400

    extracted rip-ext-build to separate executable

---
+++ lib/rip/ext.rb
@@ -0,0 +1,14 @@
+def current_version_path(path)
+  "#{path}/.versions/#{Rip.md5(Rip.ruby)}"
+end
+
+def version_path(path)
+  "#{path}/.versions/#{Rip.md5(ruby_version(path))}"
+end
+
+def ruby_version(pkg)
+  File.read(pkg+'/build.rip').chomp
+rescue Errno::ENOENT
+  warn "Package `#{basename(pkg)}' is missing build.rip and needs to be rebuilt"
+  nil
+end
